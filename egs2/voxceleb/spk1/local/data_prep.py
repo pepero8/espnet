c8f1dc31a0b8da9c7c4536da2c46aeeff07f5490
Jaehwan Lee <jhwan98@postech.ac.kr>
2024-07-14 02:23:43 +0000

    add code converting m4a to wav

--- egs2/voxceleb/spk1/local/data_prep.py
+++ egs2/voxceleb/spk1/local/data_prep.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import sys
 
+from pydub import AudioSegment
+
 
 def main(args):
     src = args.src
@@ -13,10 +15,21 @@ def main(args):
 
     for r, ds, fs in os.walk(src):
         for f in fs:
-            if os.path.splitext(f)[1] != ".wav":
-                continue
+            file_extension = os.path.splitext(f)[1]
+            # if os.path.splitext(f)[1] != ".wav":
+            if file_extension != ".wav":
+                if file_extension == ".m4a":
+                    # Convert m4a to wav
+                    m4a_path = os.path.join(r, f)
+                    wav_path = os.path.join(r, os.path.splitext(f)[0] + ".wav")
+                    audio = AudioSegment.from_file(m4a_path, format="m4a")
+                    audio.export(wav_path, format="wav")
+                    utt_dir = wav_path
+                else:
+                    continue
+            else:
+                utt_dir = os.path.join(r, f)
 
-            utt_dir = os.path.join(r, f)
             spk, vid, utt = utt_dir.split("/")[-3:]  # speaker, video, utterance
             utt_id = "/".join([spk, vid, utt.split(".")[0]])
             if spk not in spk2utt:
@@ -25,9 +38,11 @@ def main(args):
             utt2spk.append([utt_id, spk])
             wav_list.append([utt_id, utt_dir])
 
-    with open(os.path.join(dst, "spk2utt"), "w") as f_spk2utt, open(
-        os.path.join(dst, "utt2spk"), "w"
-    ) as f_utt2spk, open(os.path.join(dst, "wav.scp"), "w") as f_wav:
+    with (
+        open(os.path.join(dst, "spk2utt"), "w") as f_spk2utt,
+        open(os.path.join(dst, "utt2spk"), "w") as f_utt2spk,
+        open(os.path.join(dst, "wav.scp"), "w") as f_wav,
+    ):
         for spk in spk2utt:
             f_spk2utt.write(f"{spk}")
             for utt in spk2utt[spk]:
