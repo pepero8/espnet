7503986d0b5ef77a0d59911e5260c22778195302
jaehwan <jhwan98@postech.ac.kr>
2024-10-17 10:43:17 +0000

    code updates for ease for speaker task

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = {
         "setuptools>=38.5.1",
         "packaging",
         "configargparse>=1.2.1",
-        "typeguard",
+        "typeguard==4.3.0",
         "humanfriendly",
         "scipy>=1.4.1",
         "filelock",
@@ -25,7 +25,7 @@ requirements = {
         "torch_complex",
         "nltk>=3.4.5",
         # fix CI error due to the use of deprecated aliases
-        "numpy<1.24",
+        "numpy",
         # https://github.com/espnet/espnet/runs/6646737793?check_suite_focus=true#step:8:7651
         "protobuf",
         "hydra-core",
